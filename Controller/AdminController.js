1248f6730c688d33c2be4ddf9e6c274cc528c303
jhaharishankar <harishankarjha21@gmail.com>
2024-10-07 13:01:00 +0530

    Edit

--- Controller/AdminController.js
+++ Controller/AdminController.js
@@ -1,34 +1,60 @@
 const UserModel = require('../models/user')
 
-class AdminController{
-    static dashboard = async(req,res)=>{
+class AdminController {
+    static dashboard = async (req, res) => {
         try {
             res.render('admin/dashboard')
         } catch (error) {
             console.log(error)
         }
     }
-    static studentDisplay = async(req,res)=>{
+    static studentDisplay = async (req, res) => {
         try {
             const data = await UserModel.find()
             // console.log(data)
-            res.render('admin/studentDisplay',{d:data})
+            res.render('admin/studentDisplay', { d: data })
         } catch (error) {
             console.log(error)
         }
     }
-    static studentView = async(req,res)=>{
+    static studentView = async (req, res) => {
         try {
             // console.log(req.params.id)
             const id = req.params.id
             const data = await UserModel.findById(id)
             // console.log(data)
-            res.render('admin/studentView',{d:data})
+            res.render('admin/studentView', { d: data })
         } catch (error) {
             console.log(error)
         }
     }
-    static studentDelete = async(req,res)=>{
+    static studentEdit = async (req, res) => {
+        try {
+            // console.log(req.params.id)
+            const id = req.params.id
+            const data = await UserModel.findById(id)
+            // console.log(data)
+            res.render('admin/studentEdit', { d: data })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    static studentUpdate = async (req, res) => {
+        try {
+            // console.log(req.body)
+            let id = req.params.id
+            const { name,email,password} = req.body
+            await UserModel.findByIdAndUpdate(id,{
+                name,
+                email,
+                password
+            })
+            res.redirect('/admin/studentDisplay')
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    static studentDelete = async (req, res) => {
         try {
             // console.log(req.params.id)
             const id = req.params.id
@@ -39,4 +65,4 @@ class AdminController{
         }
     }
 }
-module.exports =AdminController
+module.exports = AdminController
